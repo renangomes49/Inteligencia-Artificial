d547735ef807bbe4f528a3485414fa2ba99f6092
renan <renan@DESKTOP-KDAHVI0>
2021-12-22 23:18:15 -0300

    Busca Em Largura

---
+++ BuscaEmLarguraMapaRomenia/src/Grafo.java
@@ -0,0 +1,158 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Grafo {
+
+	private List<Vertice> vertices = new ArrayList<Vertice>();
+	private List<Aresta> arestas = new ArrayList<Aresta>();
+
+	public List<Vertice> getVertices() {
+		return vertices;
+	}
+
+	public void setVertices(List<Vertice> vertices) {
+		this.vertices = vertices;
+	}
+
+	public List<Aresta> getArestas() {
+		return arestas;
+	}
+
+	public void setArestas(List<Aresta> arestas) {
+		this.arestas = arestas;
+	}
+	
+public No buscaEmLargura(String cidade) {
+		
+		Vertice inicio = this.encontrarVertice(cidade);
+		
+		No no = new No(inicio, null, 0);
+		
+		if(no.getEstado().getCidade().equals("Bucharest")) {
+			return no;
+		}
+		
+		Queue<No> borda = new LinkedList<No>();
+		List<No> explorados = new ArrayList<No>();
+				
+		borda.add(no);
+		
+		while(borda.size() > 0) {
+			
+			No pai = borda.remove();
+			
+			explorados.add(pai);
+			
+			for (Vertice vertice : pai.getEstado().getVerticesAdjacentes()) {
+				
+				int valorAresta = valorAresta(pai.getEstado(), vertice);
+				
+				No filho = new No(vertice, pai, pai.getCustoCaminho() + valorAresta);
+				
+				if(!explorados.contains(filho) || !borda.contains(filho)) {
+					
+					if(filho.getEstado().getCidade().equals("Bucharest")) {
+						return filho;
+					}
+					borda.add(filho);
+				}
+				
+			}
+			
+		}
+		
+		
+		return null;
+	}
+	
+	
+	public void adicionarVertice(String cidade) {
+		
+		Vertice v = new Vertice(cidade);
+		this.vertices.add(v);
+	}
+	
+	public void adicionarAresta(int peso, String cidadeInicio, String cidadeFim) {
+		
+		Vertice v1 = this.encontrarVertice(cidadeInicio);
+		Vertice v2 = this.encontrarVertice(cidadeFim);
+		
+		v1.adicionarVerticeAdjacente(v2);
+		
+		Aresta aresta = new Aresta(peso, v1, v2);
+		
+		this.arestas.add(aresta);
+		
+	}
+	
+	public Vertice encontrarVertice(String vertice) {
+		
+		for (Vertice v2 : this.vertices) {
+			if(v2.getCidade().equals(vertice)) {
+				return v2; 
+			}
+		}
+		return null;
+	}
+	
+	
+	public int valorAresta(Vertice v1, Vertice v2) {
+		
+		for (Aresta aresta : arestas) {
+			if(aresta.getCidadeInicio().getCidade().equals(v1.getCidade()) && 
+			   aresta.getCidadeFim().getCidade().equals(v2.getCidade())) {
+				
+				return aresta.getPeso();
+			}
+		}
+		
+		return 0;
+	}
+	
+	
+	public String descreverCaminho(String cidade) {
+		
+		No no = this.buscaEmLargura(cidade);
+		Stack<No> pilha = new Stack<>();
+		String caminho = "";
+		
+		while(no != null) {
+			pilha.add(no);
+			no = no.getPai();
+		}
+		
+		
+		while(!pilha.isEmpty()) {
+			No no2 = pilha.pop();
+			caminho += no2.getEstado().getCidade() + " => ";
+		}
+		return caminho;
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
